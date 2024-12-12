db5efc20ac176d707dd4be8c41caf7b9a48f38e4
Turcu Filip <turcuianis@gmail.com>
2024-12-12 14:38:19 +0200

    Create project

---
+++ MicroSocialPlatform/MicroSocialPlatform/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using MicroSocialPlatform.Data;
+using MicroSocialPlatform.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace MicroSocialPlatform.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Search(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return View(new List<ApplicationUser>());
+            }
+
+            var users = db.Users
+                          .Where(u => u.FirstName.Contains(query) || u.LastName.Contains(query) || (u.FirstName + " " + u.LastName).Contains(query))
+                          .ToList();
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Profile(string id)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var isPublicProfile = true; // Assume a property or logic to check if the profile is public
+
+            if (isPublicProfile)
+            {
+                return View(user);
+            }
+            else
+            {
+                var basicInfo = new
+                {
+                    user.FirstName,
+                    user.LastName,
+                    user.Description,
+                    user.ProfilePicture
+                };
+                return View("BasicProfile", basicInfo);
+            }
+        }
+    }
+}
---
+++ MicroSocialPlatform/MicroSocialPlatform/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MicroSocialPlatform.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? ProfilePicture { get; set; }
+        public string? Description { get; set; }
+        public bool? IsPublic { get; set; }
+    }
+}
